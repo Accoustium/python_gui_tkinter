37eaeacf29b13379637dd4650d195b576ff0a04a
Accoustium <t.pogue.python@gmail.com>
2021-03-10 13:02:07 -0600

    Create a login box

--- src/root.py
+++ src/root.py
@@ -1,10 +1,10 @@
 import tkinter as tk
-from frame.frame import Frame
+from .frame.frame import Frame
 
 
 class Root:
     def __init__(self, title: str, frame: Frame = None):
-        self.App = tk.Tk()
+        self.Window = tk.Tk()
         self.update_title(title)
         if frame is None:
             self.Frame = Frame('Base')
@@ -22,7 +22,35 @@ class Root:
 
     def update_title(self, title: str):
         self.__dict__['Title'] = title
-        self.App.title(title)
+        self.Window.title(title)
 
     def run(self):
-        self.App.mainloop()
+        self.Window.mainloop()
+
+
+class Login:
+    user: str
+    psword: str
+
+    def __init__(self):
+        self.Window = tk.Tk()
+        self.Window.title("Login")
+
+        self.user_var = tk.StringVar()
+        self.ps_var = tk.StringVar()
+
+        self.user_label = tk.Label(self.Window, text="Username").grid(column=0, row=0)
+        self.user_variable = tk.Entry(self.Window, textvariable=self.user_var, width=30).grid(column=1, row=0)
+        self.pass_label = tk.Label(self.Window, text="Password").grid(column=0, row=1)
+        self.pass_variable = tk.Entry(self.Window, textvariable=self.ps_var, width=30).grid(column=1, row=1)
+        self.submit_btn = tk.Button(self.Window, command=self.populate_password, text="Login", height=2, width=20).grid(column=3)
+
+    def __repr__(self):
+        return "LoginWindow()"
+
+    def get(self):
+        return self.user, self.psword
+
+    def populate_password(self):
+        self.user = self.user_var.get()
+        self.psword = self.ps_var.get()
